0ae8e2cd1797e6ffb6e69cf8cd4795480b996a9b
evrenvural <evrenvural4@gmail.com>
2019-01-02 17:02:18 +0300

    Switch is usable now

--- app/src/main/java/com/evrenvural/arcsincos/MainActivity.java
+++ app/src/main/java/com/evrenvural/arcsincos/MainActivity.java
@@ -3,11 +3,14 @@ package com.evrenvural.arcsincos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     private String array_spinner[];
+    private Switch mySwitch;
 
     Spinner spFonkiyonlar;
 
@@ -15,15 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinnerEkle();
+        mySwitch = findViewById(R.id.mySwitch);
+        mySwitch.setChecked(true);
+        spinnerEkle(true);
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                spinnerEkle(isChecked);
+            }
+        });
+
+
     }
 
-    void spinnerEkle(){
-        int arrayUzunluk = getResources().getStringArray(R.array.fonksiyonlar).length;
-        array_spinner = new String[arrayUzunluk];
-        array_spinner = getResources().getStringArray(R.array.fonksiyonlar);
-        spFonkiyonlar = findViewById(R.id.spFonksiyonlar);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,array_spinner);
-        spFonkiyonlar.setAdapter(arrayAdapter);
+    void spinnerEkle(boolean tersMi){
+       if (tersMi){
+           int arrayUzunluk = getResources().getStringArray(R.array.ters_fonksiyonlar).length;
+           array_spinner = new String[arrayUzunluk];
+           array_spinner = getResources().getStringArray(R.array.ters_fonksiyonlar);
+           spFonkiyonlar = findViewById(R.id.spFonksiyonlar);
+           ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,array_spinner);
+           spFonkiyonlar.setAdapter(arrayAdapter);
+       }
+       else{
+           int arrayUzunluk = getResources().getStringArray(R.array.fonksiyonlar).length;
+           array_spinner = new String[arrayUzunluk];
+           array_spinner = getResources().getStringArray(R.array.fonksiyonlar);
+           spFonkiyonlar = findViewById(R.id.spFonksiyonlar);
+           ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,array_spinner);
+           spFonkiyonlar.setAdapter(arrayAdapter);
+       }
     }
 }
