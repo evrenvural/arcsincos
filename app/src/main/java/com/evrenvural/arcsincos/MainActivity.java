af459a188aa6f95f3380754668ca266415a204c1
evrenvural <evrenvural4@gmail.com>
2019-01-02 15:35:32 +0300

    Some changes were made on the UI

--- app/src/main/java/com/evrenvural/arcsincos/MainActivity.java
+++ app/src/main/java/com/evrenvural/arcsincos/MainActivity.java
@@ -2,12 +2,27 @@ package com.evrenvural.arcsincos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String array_spinner[];
+
+    Spinner spFonkiyonlar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerEkle();
+    }
+
+    void spinnerEkle(){
+        array_spinner = new String[4];
+        array_spinner = getResources().getStringArray(R.array.fonksiyonlar);
+        spFonkiyonlar = findViewById(R.id.spFonksiyonlar);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,array_spinner);
+        spFonkiyonlar.setAdapter(arrayAdapter);
     }
 }
